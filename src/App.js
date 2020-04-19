78d3466939e4a30c8299e9dbdeb0ad06d74f4d32
Paula Suarez <paula.suarez@freeman.com>
2020-04-19 18:24:27 -0500

    Adding updates to app.js file

--- src/App.js
+++ src/App.js
@@ -35,17 +35,24 @@ class App extends Component {
 
       return (
         <div className="App">
-
+          <div class="jumbotron">
+          <div class="card-header">
           <h1>Films</h1>
-          <ul>
-            {items.map(item => (
-              <li key={item.id}>
-                Title: {item.title} |
-                Description: {item.description}  |
-                Director: {item.director} }
-              </li>
-            ))};
+          </div>
+          <div class="card">
+              <ul class="list-group list-group-flush">
+                {items.map(item => (
+                  <li class="list-group-item" key={item.id}>
+                   <b>Title:</b>  {item.title} 
+                   <div><br /></div>
+                <b>Description:</b> {item.description}  
+                  </li>
+                ))};   
+
           </ul>
+          </div>
+
+          </div >
         </div>
 
       );
