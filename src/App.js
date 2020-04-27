5cbbee02653fb3d032cefe6ae76027bceb8cbfff
Paula Suarez <paula.suarez@freeman.com>
2020-04-27 13:12:42 -0500

    Adding updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import 'es6-promise';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 class App extends Component {
 
@@ -11,7 +15,17 @@ class App extends Component {
       items: [],
       isLoaded: false,
 
-    }
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+     
+
+  }
+
+  handleClick(e) {
+    this.setState({
+      loaded: true
+    });
   }
 
   componentDidMount() {
@@ -19,38 +33,50 @@ class App extends Component {
       .then(res => res.json())
       .then(json => {
         this.setState({
-          isLoaded: true,
+          isLoaded: false,
           items: json,
+         
         })
       });
   }
 
+
+
   render() {
 
+    
     var { isLoaded, items } = this.state;
     if (!isLoaded) {
       return <div>Loading...</div>;
+     
     }
     else {
 
       return (
-        <div className="App">
-          <div class="jumbotron">
-          <div class="card-header">
-          <h1>Films</h1>
-          </div>
-          <div class="card">
-              <ul class="list-group list-group-flush">
-                {items.map(item => (
-                  <li class="list-group-item" key={item.id}>
-                   <b>Title:</b>  {item.title} 
-                   <div><br /></div>
-                <b>Description:</b> {item.description}  
-                  </li>
-                ))};   
+       
 
-          </ul>
-          </div>
+        <div className="App">
+          <div className="jumbotron">
+            <div>
+              <br />
+              <button onClick={this.handleClick}>Click Me!</button>
+
+            </div>
+            <Card.Header>
+              <h1>films</h1>
+            </Card.Header>
+            <div className="card">
+              <ListGroup variant="flush">
+                {items.map(item => (
+                  <ListGroup.Item key={item.id}>
+                    <b>Title:</b>  {item.title}
+                    <div><br /></div>
+                    <b>Description:</b> {item.description}
+                  </ListGroup.Item>
+                ))};
+
+          </ListGroup>
+            </div>
 
           </div >
         </div>
